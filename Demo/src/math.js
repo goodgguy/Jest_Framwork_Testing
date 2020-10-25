b916773a6ef2da9412c57876402d1af7fe367000
Hoang Tuan <34570205+goodgguy@users.noreply.github.com>
2020-10-26 04:03:45 +0700

    ok

---
+++ Demo/src/math.js
@@ -0,0 +1,11 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => b - a;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => b / a;
+module.exports =
+{
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide
+}
